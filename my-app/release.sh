5a66c362b4af4620045ea460f34995aefb12879d
FilipeCosta <filipecostaa10@gmail.com>
2024-04-26 15:56:39 +0200

    asdas

--- my-app/release.sh
+++ my-app/release.sh
@@ -68,7 +68,7 @@ if [[ "$response" =~ ^[Yy]$ ]]; then
     git add .
     git commit -m "Release ${new_tag}"
     git tag $new_tag
-    git push origin $new_tag
+    git push -f origin $new_tag
     git checkout main
 
     echo -e "\n${GREEN}Release completed successfully${NC} - ${releases_remote_url}${new_tag}"
