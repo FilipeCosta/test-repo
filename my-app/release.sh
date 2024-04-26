d32acc6f5a00a940b134c5f2986cbdc1f728c4b9
FilipeCosta <filipecostaa10@gmail.com>
2024-04-26 16:29:08 +0200

    new commit

--- my-app/release.sh
+++ my-app/release.sh
@@ -69,8 +69,8 @@ if [[ "$response" =~ ^[Yy]$ ]]; then
     mv /tmp/build .
     git add .
     git commit -m "Release ${new_tag}"
-    git tag $new_tag
-    git push origin $new_tag
+    git tag -f $new_tag
+    git push origin -f $new_tag
     git checkout main
 
     echo -e "\n${GREEN}Release completed successfully${NC} - ${releases_remote_url}${new_tag}"
