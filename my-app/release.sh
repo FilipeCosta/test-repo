2c46a407a7de5133727fb55cac96224be308e288
FilipeCosta <filipecostaa10@gmail.com>
2024-04-26 11:25:05 +0200

    ch

--- my-app/release.sh
+++ my-app/release.sh
@@ -47,20 +47,17 @@ if [ -n "$latest_tag" ]; then
     new_tag=$(echo "$latest_tag" | sed -E 's/v([0-9]+)\.([0-9]+)\.([0-9]+)/printf "v\1.\2.$((\3 + 1))"/ge')
 fi
 
-# Check if pnpm is available
-if ! command -v pnpm >/dev/null 2>&1; then
-    echo -e "${RED}You don't have pnpm installed${NC}"
-    exit 1
-else
-    pnpm build
-    git checkout release
-fi
-
 echo "Do you want to publish tag ${new_tag}? (yes/no)"
 read response
 
 if [[ "$response" =~ ^[Yy][Ee][Ss]$ ]]; then
-    echo "User confirmed. Proceeding..."
+    if ! command -v pnpm >/dev/null 2>&1; then
+        echo -e "${RED}You don't have pnpm installed${NC}"
+        exit 1
+    else
+        pnpm build
+        git checkout release
+    fi
 else
     echo "Operation canceled"
 fi
